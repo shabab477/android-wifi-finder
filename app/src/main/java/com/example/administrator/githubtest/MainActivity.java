4b06e2b026526ba8c1fd5656b302c3d575ad84dd
shabab477 <shababkarim93@gmail.com>
2016-04-13 17:16:02 +0600

    done showing wifis

--- app/src/main/java/com/example/administrator/githubtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/githubtest/MainActivity.java
@@ -1,16 +1,94 @@
 package com.example.administrator.githubtest;
 
+//author Shabab
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    WifiManager wifi;
+    ListView lv;
+    TextView textStatus;
+    Button buttonScan;
+    int size = 0;
+    List<ScanResult> results;
+
+    String ITEM_KEY = "key";
+    ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter;
+
+    /* Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //textStatus = (TextView) findViewById(R.id.textStatus);
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(this);
+        lv = (ListView)findViewById(R.id.mobile_list);
 
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifi.isWifiEnabled() == false)
+        {
+            Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+            wifi.setWifiEnabled(true);
+        }
+        this.adapter = new SimpleAdapter(this, arraylist, R.layout.row, new String[] { ITEM_KEY }, new int[]{ R.id.item1});
+        lv.setAdapter(this.adapter);
+
+        registerReceiver(new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context c, Intent intent)
+            {
+                results = wifi.getScanResults();
+                size = results.size();
+            }
+        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+    }
+
+    public void onClick(View view)
+    {
+        arraylist.clear();
+        wifi.startScan();
+
+        //Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
+        try
+        {
+            size = size - 1;
+            while (size >= 0)
+            {
+                HashMap<String, String> item = new HashMap<String, String>();
+                item.put(ITEM_KEY, results.get(size).SSID + "  " + results.get(size).capabilities);
+
+                arraylist.add(item);
+                size--;
+                adapter.notifyDataSetChanged();
+            }
+        }
+        catch (Exception e)
+        { }
     }
 }
